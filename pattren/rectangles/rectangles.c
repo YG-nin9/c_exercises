5cb67f9ff6edfbf3e3e4a659722a860909b53686
YG-nin9 <youssef.ghatas20@gmail.com>
2023-10-08 02:21:05 -0700

    dip

---
+++ pattren/rectangles/rectangles.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void solid_rectangle(int row, int column);
+void hollow_rectangle(int row, int column);
+
+int main()
+{
+    int row, column;
+
+    printf("Enter the row number: \n");
+    scanf("%i", &row);
+    printf("Enter the column number: \n");
+    scanf("%i", &column);
+
+    hollow_rectangle(row, column);
+    printf("\n \n \n");
+    solid_rectangle(row, column);
+
+    return 0;
+}
+
+
+
+void solid_rectangle(int row, int column){
+
+    int rr = 0;
+    int cc;
+
+    for(;rr < row; rr++){
+
+        for (cc = 0;cc < column; cc++){
+            putchar('*');
+        }
+
+        putchar('\n');
+    }
+}
+
+
+void hollow_rectangle(int row, int column){
+
+    int rr = 0;
+    int cc;
+
+    for(;rr < row; rr++){
+
+        for (cc = 0;cc < column; cc++){
+
+            if ((rr == 0) || (rr == row - 1)){
+                putchar('*');
+            }
+            else{
+
+                if ((cc == 0) || (cc == column - 1)){
+                    putchar('*');
+                }
+                else {
+                    putchar(' ');
+                }
+            }
+        }
+
+        putchar('\n');
+    }
+}
+
